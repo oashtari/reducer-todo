77c5521307d7897ca39596cc29ab7f29a060708a
oashtari <oashtari@gmail.com>
2020-02-01 20:58:21 -0800

    finished step 1, created reducer file with initialState and todoReducer

---
+++ reducer_todo_project/src/reducers/index.js
@@ -0,0 +1,11 @@
+export const initialState = {
+    item: 'Learn about reducers',
+    complete: false,
+    id: 3892987589
+};
+
+export const todoReducer = (state, action) => {
+    
+    default:
+return state;
+}
